d749aab31257126b0637e0ccdf507be0b41803ce
Sumira Naroola <mac6@129mac18.valleychristian.net>
2017-01-25 10:54:11 -0800

    added day/night button change

--- webapp.js
+++ webapp.js
@@ -1,5 +1,7 @@
 // JavaScript Document
 
 document.write("You wake up in a dark, dreary cave. You have no memory of your previous life, and the only thing in your mind is the echo of water dripping from the ceiling.");
-
 var name = prompt("What is your name?")
+
+document.write("You see two things in the cave: A sword and a backage of bread. Which do you take? Only one will fit in your backpack.");
+
